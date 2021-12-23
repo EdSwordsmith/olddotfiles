6fd120cfb4da78988fa8c2d6c8569f81e2624be1
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2021-12-23 14:20:18 +0000

    moved fish to nix

--- fish/.config/fish/functions/cat.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function cat --wraps='bat --paging=never --style=plain' --wraps='bat --theme="Solarized (dark)" --paging=never --style=plain' --description 'alias cat=bat --theme="Solarized (dark)" --paging=never --style=plain'
-  bat --theme="Solarized (dark)" --paging=never --style=plain $argv; 
-end
--- fish/.config/fish/functions/doom.fish
+++
@@ -1,4 +0,0 @@
-# Defined via `source`
-function doom --wraps='~/.emacs.d/bin/doom' --description 'alias doom=~/.emacs.d/bin/doom'
-  ~/.emacs.d/bin/doom $argv; 
-end
